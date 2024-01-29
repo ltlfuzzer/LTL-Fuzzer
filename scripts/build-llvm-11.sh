ea8495cc787330de7f88c00ca2e62ca89d0446a8
Jiefeng Li <xerath7d0@gmail.com>
2024-01-29 21:57:00 +0800

    fix incorrect command order

--- scripts/build-llvm-11.sh
+++ scripts/build-llvm-11.sh
@@ -2,9 +2,9 @@
 
 sudo echo deb http://apt.llvm.org/bionic/ llvm-toolchain-bionic-11 main >> /etc/apt/sources.list
 sudo echo deb-src http://apt.llvm.org/bionic/ llvm-toolchain-bionic-11 main >> /etc/apt/sources.list
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
 sudo apt update
 sudo apt upgrade
-wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
 sudo apt install clang-11 llvm-11 llvm-11-dev lldb-11 lld-11 -y
 sudo apt install libc++-11-dev libc++abi-11-dev -y
 sudo ln -s /usr/bin/clang-11 /usr/bin/clang
